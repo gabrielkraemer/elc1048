d2b616890cda6b940b4d80192ff329bc6c7cb892
gabriel_eisenkraemer <47396272+gabriel_eisenkraemer@users.noreply.github.com>
2020-04-30 23:37:17 -0300

    Exercicio 2
    Cria uma tarefa periódica de periodo 100ms, coloquei pra executar em modo preemptivo e cooperativo.

--- MULTITAREFAS/IAR/main.c
+++ MULTITAREFAS/IAR/main.c
@@ -8,14 +8,12 @@
  */
 void tarefa_1(void);
 void tarefa_2(void);
-void tarefa_3(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
  */
 #define TAM_PILHA_1		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2		(TAM_MINIMO_PILHA + 24)
-#define TAM_PILHA_3		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -23,7 +21,6 @@ void tarefa_3(void);
  */
 uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
 uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
-uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -38,8 +35,6 @@ int main(void)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
-        
-        CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -64,7 +59,7 @@ void tarefa_1(void)
 	for(;;)
 	{
 		a++;
-		TarefaContinua(2);
+		TarefaEspera(100);
 	
 	}
 }
@@ -72,21 +67,14 @@ void tarefa_1(void)
 void tarefa_2(void)
 {
 	volatile uint16_t b = 0;
+        volatile int i, j = 0;
 	for(;;)
 	{
+                for(i = 0; i < 100000; i++){
+                        j++;
+                }
+               
 		b++;
-		TarefaSuspende(2);	
+		TarefaEspera(100);	
 	}
-}
-
-void tarefa_3(void)
-{
-
-        volatile uint16_t c = 0;
-        for(;;)
-        {
-                c++;
-                TarefaSuspende(2);
-        }
-  
 }
